938996e8d4c4d6710db992c642b236b6bf8d6b99
generalsayre <levi.is.dreaming@gmail.com>
2025-06-02 13:27:53 +0800

    adding button test

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+
+const button = document.getElementById('myButton');
+const background = document.getElementById('myBackground');
+
+button.addEventListener('click', () => {
+  background.style.backgroundImage = 'url(\'https://wallpapers.com/images/hd/plain-black-desktop-2560-x-1440-ugpl0479gu0vuwnl.jpg\')'; // 
+});
