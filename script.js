627fe52049befc926fc68e8a908cde58da3434b6
generalsayre <levi.is.dreaming@gmail.com>
2025-06-02 13:59:49 +0800

    have to remove button

--- script.js
+++ script.js
@@ -5,10 +5,3 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
-
-const button = document.getElementById('myButton');
-const background = document.getElementById('myBackground');
-
-button.addEventListener('click', () => {
-  background.style.backgroundImage = 'url(\'https://wallpapers.com/images/hd/plain-black-desktop-2560-x-1440-ugpl0479gu0vuwnl.jpg\')'; // 
-});
